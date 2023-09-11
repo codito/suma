ac8ff611db65870aa4844380d31ef77f92893e9e
Arun Mahapatra <codito@codito.in>
2023-09-11 23:58:08 +0530

    feat: add lexrank implementation.

--- suma/main.py
+++ suma/main.py
@@ -8,9 +8,12 @@ import frontmatter
 import kmedoids
 import markdown
 import nltk
+import numpy as np
 from bs4 import BeautifulSoup
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+
+from suma.lexrank import degree_centrality_scores
 
 extractive_model = SentenceTransformer("BAAI/bge-small-en")
 abstractive_model = "xxx"
@@ -34,6 +37,24 @@ def get_embeddings(phrases: List[str]):
     return extractive_model.encode(phrases)
 
 
+def get_key_sentences_lexrank(phrases: List[str], num_sentences: int) -> List[str]:
+    embeddings = get_embeddings(phrases)
+    sim_matrix = cosine_similarity(embeddings)
+
+    centrality_scores = degree_centrality_scores(sim_matrix, threshold=None)
+    most_central_indices = np.argsort(-centrality_scores)[:num_sentences]
+    return [phrases[i] for i in most_central_indices]
+
+
+def get_key_sentences_kmedoid(phrases: List[str], num_sentences: int) -> List[str]:
+    embeddings = get_embeddings(phrases)
+    sim_matrix = cosine_similarity(embeddings)
+
+    km = kmedoids.KMedoids(num_sentences, method="fasterpam")
+    km.fit(sim_matrix)
+    return [phrases[i] for i in sorted(km.medoid_indices_)]
+
+
 def summarize(args: List[str]):
     if len(args) < 1:
         print("Usage: suma <text file>")
@@ -43,15 +64,13 @@ def summarize(args: List[str]):
         nltk.download("punkt")
 
     sentences = extract_sentences(args[0])
-    embeddings = get_embeddings(sentences)
-    sim_matrix = cosine_similarity(embeddings)
-
     num_clusters = int(len(sentences) / 5)
-    km = kmedoids.KMedoids(num_clusters, method="fasterpam")
-    km.fit(sim_matrix)
+    key_sentences_kmedoid = get_key_sentences_kmedoid(sentences, num_clusters)
+    key_sentences_lexrank = get_key_sentences_lexrank(sentences, num_clusters)
 
-    for index in sorted(km.medoid_indices_):
-        print(sentences[index])
+    print(key_sentences_kmedoid)
+    print("----------")
+    print(key_sentences_lexrank)
 
     pass
 
